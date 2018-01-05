5d5294fb18e0a3112c8c51af4dcb84636c13ecf6
unknown <dj@psfsi.com>
2018-01-05 21:15:28 +0100

    moj prvi commit

---
+++ app/src/main/java/com/example/code/vezbanje/drugo_dugme.java
@@ -0,0 +1,13 @@
+package com.example.code.vezbanje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class drugo_dugme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drugo_dugme);
+    }
+}
